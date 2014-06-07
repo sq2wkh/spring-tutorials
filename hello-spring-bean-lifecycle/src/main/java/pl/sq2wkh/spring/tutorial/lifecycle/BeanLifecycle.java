c984aba331d8787c7e3b6ed9c947227a26d86c76
adam <sq2wkh@gmail.com>
2014-06-08 00:09:51 +0200

    dodanie nastepnego tutoriala i wyrzucenie niepotrzebnej deklaracji bibliotek w pomach

---
+++ hello-spring-bean-lifecycle/src/main/java/pl/sq2wkh/spring/tutorial/lifecycle/BeanLifecycle.java
@@ -0,0 +1,21 @@
+package pl.sq2wkh.spring.tutorial.lifecycle;
+
+/**
+ * Created by adam on 08.06.14.
+ */
+public class BeanLifecycle {
+    private String message;
+
+    public void setMessage(String message){
+        this.message  = message;
+    }
+    public void getMessage(){
+        System.out.println("Your Message : " + message);
+    }
+    public void init(){
+        System.out.println("Bean is going through init.");
+    }
+    public void destroy(){
+        System.out.println("Bean will destroy now.");
+    }
+}
